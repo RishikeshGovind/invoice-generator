6ac25b1d5f85a4abdc6faa3fbe1a2e1590c609e5
RishikeshGovind <aethernalytics@gmail.com>
2025-07-18 22:36:56 +0100

    deployment using github pages

--- invoice-generator-client/vite.config.js
+++ invoice-generator-client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // When deployed to https://<username>.github.io/invoice-generator/
+  base: '/invoice-generator/',
   plugins: [react()],
+
+  // proxy API calls to your Spring Boot server in dev
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
